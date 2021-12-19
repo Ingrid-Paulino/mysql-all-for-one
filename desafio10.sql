3f7809079f82aded35b54fcba4a8b289f453d545
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-12-19 10:09:34 -0300

    C28 - passando req 10 e 11

--- desafio10.sql
+++ desafio10.sql
@@ -1,3 +1,7 @@
-SELECT id, created_by FROM northwind.purchase_orders
+-- forma1
+SELECT * FROM northwind.purchase_orders
 WHERE created_by >= 3
 ORDER BY id ASC, created_by DESC;
+
+-- forma2
+-- SELECT * FROM northwind.purchase_orders WHERE (created_by = 3 OR created_by > 3) ORDER BY created_by DESC, id;
--- desafio11.sql
+++ desafio11.sql
@@ -1,2 +1,12 @@
-SELECT notes FROM northwind.purchase_orders
-WHERE VALUE >= 30 AND VALUE <= 39;
+-- forma1
+-- SELECT notes FROM purchase_orders WHERE notes BETWEEN 'Purchase generated based on Order #30' AND 'Purchase generated based on Order #39';
+
+-- forma2
+-- SELECT notes FROM northwind.purchase_orders WHERE notes LIKE '%#3_';
+
+-- forma3
+SELECT notes FROM northwind.purchase_orders WHERE notes LIKE 'Purchase generated based on Order #3_';
+
+-- forma4
+-- SELECT notes FROM purchase_orders
+-- WHERE notes LIKE '%3%';
